3d6f836e06fc1eb0aa4b634f48c18f87f200efdb
Michael Chen <dattt.piass@gmail.com>
2025-07-08 17:34:24 +0700

    Using Multiple Terraform Providers

--- terraform.tf
+++ terraform.tf
@@ -9,5 +9,13 @@ terraform {
       source  = "hashicorp/random"
       version = "3.1.0"
     }
+    http = {
+      source = "hashicorp/http"
+      version = "2.1.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
+    }
   }
 }
