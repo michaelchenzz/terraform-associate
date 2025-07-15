6bbe580cf5f73847d5c446432269cd4a9122e8ce
Michael Chen <dattt.piass@gmail.com>
2025-07-15 16:20:21 +0700

    Terraform Modules - Inputs and Outputs

--- main.tf
+++ main.tf
@@ -265,6 +265,7 @@ resource "aws_security_group" "vpc-ping" {
 module "server" {
   source    = "./modules/server"
   ami       = data.aws_ami.ubuntu.id
+  size      = "t3.micro"
   subnet_id = aws_subnet.public_subnets["public_subnet_3"].id
   security_groups = [
     aws_security_group.vpc-ping.id,
@@ -287,29 +288,6 @@ module "server_subnet_1" {
   ]
 }
 
-module "autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "8.0.1"
-
-  # Autoscaling group
-  name = "myasg"
-
-  vpc_zone_identifier = [
-    aws_subnet.private_subnets["private_subnet_1"].id,
-    aws_subnet.private_subnets["private_subnet_2"].id,
-    aws_subnet.private_subnets["private_subnet_3"].id
-  ]
-  min_size         = 0
-  max_size         = 1
-  desired_capacity = 1
-
-  # Launch template
-  image_id      = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-  instance_name = "asg-instance"
-
-  tags = {
-    Name = "Web EC2 Server 2"
-  }
-
+output "size" {
+  value = module.server.size
 }
