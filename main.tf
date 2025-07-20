3e476513b65f87f8e8f9852699fdade3e0a92ae6
Michael Chen <dattt.piass@gmail.com>
2025-07-20 16:47:54 +0700

    Use the Core Terraform Workflow

--- main.tf
+++ main.tf
@@ -138,7 +138,7 @@ data "aws_ami" "ubuntu" {
 # Terraform Resource Block - To Build EC2 instance in Public Subnet
 resource "aws_instance" "web_server" {
   ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t3.micro"
+  instance_type               = "t3.small"
   subnet_id                   = aws_subnet.public_subnets["public_subnet_1"].id
   security_groups             = [aws_security_group.vpc-ping.id, aws_security_group.ingress-ssh.id, aws_security_group.vpc-web.id]
   associate_public_ip_address = true
