08b4deef6b063c4c1c58987405d7d698f7f6e275
Michael Chen <dattt.piass@gmail.com>
2025-08-01 17:31:26 +0700

    Terraform Outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,18 @@ output "vpc_information" {
   description = "VPC Information about Environment"
   value       = "Your ${aws_vpc.vpc.tags.Environment} VPC has an ID of ${aws_vpc.vpc.id}"
 }
+
+output "public_ip" {
+  description = "This is the public IP of my web server"
+  value       = aws_instance.web_server.public_ip
+}
+
+output "public_url" {
+  description = "Public URL for our Web Server"
+  value       = "https://${aws_instance.web_server.private_ip}:8080/index.html"
+}
+
+output "ec2_instance_arn" {
+  value     = aws_instance.web_server.arn
+  sensitive = true
+}
